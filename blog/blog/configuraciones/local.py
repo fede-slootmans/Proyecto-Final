caf3bc1579b972ae44dfe977d180dad7fcdf0eb1
Rios Lucas Martin <lucas.rios@nbch.com.ar>
2024-10-14 20:35:12 -0300

    ambientation to deploy in srv

--- blog/blog/configuraciones/local.py
+++ blog/blog/configuraciones/local.py
@@ -1,13 +1,19 @@
 from .settings import *
 
 DEBUG = True
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'blogdb',
+#         'USER': 'root',
+#         'PASSWORD': '123456789',
+#         'HOST': 'localhost',
+#         'PORT': '3306',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'blogdb',
-        'USER': 'root',
-        'PASSWORD': '123456789',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
